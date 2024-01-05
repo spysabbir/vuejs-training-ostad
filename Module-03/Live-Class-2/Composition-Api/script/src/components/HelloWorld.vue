f4a721d197509d7aacf77786de9acb48960f95ae
spysabbir <sabbirahammedsovon@gmail.com>
2024-01-05 12:31:07 +0600

    Module 03 || Live Class-2 Done

---
+++ Module-03/Live-Class-2/Composition-Api/script/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  setup() {
+    return {
+      message: "Hello Component."
+    }
+  }
+}
+</script>
+
+<template>
+  <h4>{{ message }}</h4>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Module-03/Live-Class-2/Composition-Api/setup/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+  // import HelloWorld from './components/HelloWorld.vue'
+  import TagLine from './components/TagLine.vue'
+  import Hero from './components/Hero.vue'
+  import Button from './components/Button.vue'
+  import Sponsor from './components/Sponsor.vue'
+
+  const message = "Hello VueJs"
+
+  function sayHello() {
+    return "Hello Function"
+  }
+</script>
+
+<template>
+  <!-- <h3>{{ message }}</h3>
+  <h4>{{ sayHello() }}</h4>
+  <HelloWorld msg="Vite + Vue" /> -->
+
+  <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <TagLine></TagLine>
+        <Hero></Hero>
+        <Button></Button> 
+        <Sponsor></Sponsor> 
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
