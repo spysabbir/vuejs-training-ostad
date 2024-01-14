2a4e2a39e10c78b10e9422ee9bdc32815b85a2fa
spysabbir <sabbirahammedsovon@gmail.com>
2024-01-14 15:42:17 +0600

    Module 06 Done

---
+++ Module-06/Assignment/taskApp/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import Task from './components/Task.vue';
+</script>
+
+<template>
+  <section class="container mx-auto bg-slate-500 h-full w-full p-5">
+    <h1 class="text-white text-2xl text-center border-b-2 pb-2">VueJs 3 Task App </h1>
+
+    <!-- Task Component -->
+    <Task />
+
+  </section>
+</template>
+
+<style>
+
+</style>
