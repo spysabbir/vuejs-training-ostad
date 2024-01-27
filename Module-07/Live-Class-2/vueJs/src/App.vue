c96e8fdc309bda10c68057eea94698adc3edd5b6
spysabbir <sabbirahammedsovon@gmail.com>
2024-01-27 12:45:15 +0600

    Module 07 || Live Class-2 Done

---
+++ Module-07/Live-Class-2/vueJs/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import SignIn from './components/SignIn.vue'
+import Form from './components/Form.vue'
+</script>
+
+<template>
+  <h1 class="text-center text-2xl my-2"> Module 07 || Live Class 2 </h1>
+  <h2 class="text-center">Review Class</h2>
+
+  <!-- <SignIn></SignIn> -->
+
+  <!-- Form -->
+  <Form></Form>
+</template>
+
+<style scoped>
+</style>
---
+++ Module-07/Live-Class-2/vueJs/src/components/Form.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+const name = ref('VueJs 3')
+function updateValue(event) {
+    name.value = event.target.value
+}
+function search() {
+    alert(name.value)
+}
+const color = ref('color')
+</script>
+
+<template>
+<!-- Two wat data binding -->
+<!-- <p>{{ name }}</p> -->
+<!-- <input type="text" :value="name" @input="updateValue($event)"> -->
+<!-- <input type="text" v-model="name" >
+<button type="button" @click="search()">Search</button> -->
+
+<div class="">
+    {{ color }}
+</div>
+<input type="checkbox" v-model="color" true-value="red"> Red
+</template>
+
+<style>
+
+</style>
