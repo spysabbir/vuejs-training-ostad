43aedaf2b975e15f915486d8ace3512a365e0c61
spysabbir <sabbirahammedsovon@gmail.com>
2024-02-11 11:23:13 +0600

    Module 09 || Live Class-1 Done

---
+++ Module-09/Live-Class-1/vueJs/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated } from 'vue'
+  import HelloWorld from './components/HelloWorld.vue'
+
+  onBeforeMount(() => {
+    console.log('1. Parent onBeforeMount');
+  })
+
+  onMounted(() => {
+    console.log('2. Parent onMounted');
+  })
+
+  onBeforeUpdate(() => {
+    console.log('3. Parent onBeforeUpdate');
+  })
+
+  onUpdated(() => {
+    console.log('4. Parent onUpdated');
+  })
+
+  const count = ref(0)
+  const status = ref(true)
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <div class="mb-5 mx-auto">
+      <h1>Parent</h1>
+      <button type="button" class="p-2 bg-indigo-500 m-1 rounded text-white" @click="count++">Count is {{ count }}</button>
+    </div>
+  </div>
+
+  <div class="container mx-auto">
+    <div class="mb-5 mx-auto">
+      <button type="button" class="p-2 bg-indigo-500 m-1 rounded text-white" @click="status = !status">Toggle</button>
+      <HelloWorld v-if="status"></HelloWorld>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Module-09/Live-Class-1/vueJs/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<script setup>
+    import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue'
+
+    onBeforeMount(() => {
+        console.log('1. Child onBeforeMount');
+    })
+
+    onMounted(() => {
+        console.log('2. Child onMounted');
+    })
+
+    onBeforeUpdate(() => {
+        console.log('3. Child onBeforeUpdate');
+    })
+
+    onUpdated(() => {
+        console.log('4. Child onUpdated');
+    })
+
+    onBeforeUnmount(() => {
+        console.log('5. Child onBeforeUnmount');
+    })
+
+    onUnmounted(() => {
+        console.log('6. Child onUnmounted');
+    })
+
+  const count = ref(0)
+</script>
+
+<template>
+    <h1>Child</h1>
+    <button type="button" class="p-2 bg-indigo-500 m-1 rounded text-white" @click="count++">Count is {{ count }}</button>
+</template>
+
+<style scoped>
+
+</style>
