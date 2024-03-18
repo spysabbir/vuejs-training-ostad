4a6a8361c2e189e023f34446a11c60e913ef0ec2
spysabbir <sabbirahammedsovon@gmail.com>
2024-03-18 08:48:31 +0600

    Module 12 || Live Class-1 Done

---
+++ Module-12/Live-Class-1/vueJs/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+import Sidebar from './components/Sidebar.vue'
+import Navigation from './components/Navigation.vue'
+import Player from './components/Player.vue'
+import engine from './data/engine';
+engine.initialize()
+</script>
+
+<template>
+  <div class="player">
+    <Navigation />
+    <div class="main flex sm:flex-col sm:flex-col-reverse lg:flex-row">
+      <Sidebar />
+      <Player />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Module-12/Live-Class-1/vueJs/src/data/data.js
@@ -0,0 +1,14 @@
+import { reactive } from 'vue'
+
+export default reactive({
+    "id": "Ostad",
+    "title": "Ostad Title",
+    "intro": {
+        "title": "Intro Title",
+        "video": {
+            "url": "https://www.youtube.com/embed/yYRAxd6CYC8",
+            "type": "youtube",
+        },
+    },
+    "chapters": []
+})
