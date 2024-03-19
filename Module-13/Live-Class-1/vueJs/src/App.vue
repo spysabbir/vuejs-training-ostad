63ccb07624903252a5c782d2fcd5caa2735424d1
spysabbir <sabbirahammedsovon@gmail.com>
2024-03-19 12:25:54 +0600

    Module 13 Done

--- Module-13/Live-Class-1/vueJs/src/App.vue
+++ Module-13/Live-Class-1/vueJs/src/App.vue
@@ -2,6 +2,18 @@
 </script>
 
 <template>
+  <h1> Module 13 || Live Class 1 </h1>
+  <h2>Understanding Vue Router</h2>
+  <p>
+    <strong>Note: </strong>
+    What is a vue router?,
+    Workflow of vue router,
+    How to setup vue router,
+    Basic use case and setup of vue router,
+    Vue router parent routes with examples,
+    Vue router nested route with examples,
+    Vue router dynamic route with examples,
+  </p>
   <div class="mb-5 p-5 bg-slate-500">
     <ul class="flex justify-center">
       <li><router-link to="/" class="px-3 py-2 bg-orange-400 rounded m-1">Home</router-link></li>
--- Module-13/Live-Class-2/vueJs/src/App.vue
+++ Module-13/Live-Class-2/vueJs/src/App.vue
@@ -3,6 +3,13 @@ import Navber from './components/parts/Navber.vue';
 </script>
 
 <template>
+   <h1> Module 13 || Live Class 2 </h1>
+  <h2>Creating A Blog Project</h2>
+  <p>
+    <strong>Note: </strong>
+    A dynamic blog project using vue router
+  </p>
+
   <Navber/>
 
   <div class="p-6 px-24">
